70c42003599e913304be9092da43ef9dccfbdbd2
Emmanuel Ugwu <>
2021-08-11 14:48:14 +0100

    Made changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,92 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
+
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [editing, setEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name){
+      showAlert(true, 'please enter a value', 'danger')
+    } else if(name && editing){
+      setList(list.map((item) => {
+        if (item.id === editID){
+          return {...item, title: name}
+        }
+        return item
+      }))
+      setName('');
+      setEditID(null);
+      setEditing(false);
+      showAlert(true, 'value changed', 'success')
+    } else{
+      showAlert(true, 'item added to the list', 'success')
+      const newItem = {id: new Date().getTime().toString(), title: name};
+      setList([...list, newItem]);
+      setName('')
+    }
+  }
+
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({show, msg, type})
+  }
+
+  const clearList = () => {
+    showAlert(true, 'empty list', 'danger')
+    setList([])
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, 'item removed', 'danger');
+    setList(list.filter((item) => item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const mainItem = list.find((item) => item.id === id);
+    setEditing(true);
+    setEditID(id);
+    setName(mainItem.title)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
+  return (
+  <section className="section-center">
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+      <h3>to-do list</h3>
+      <div className="form-control">
+        <input type="text" className="todolist" placeholder="e.g. Read books"
+        value={name} onChange={(e) => setName(e.target.value)} />
+        <button type="submit" className="submit-btn">
+          {editing ? 'edit' : 'add'}
+        </button>
+      </div>
+    </form>
+    {list.length > 0 &&(
+      <div className="todo-container">
+      <List items={list} removeItem={removeItem} editItem={editItem} />
+      <button className="clear-btn" onClick={clearList}>
+        clear items
+      </button>
+    </div>
+    )}
+    </section>
+  )
 }
 
 export default App
